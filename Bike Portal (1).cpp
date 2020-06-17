91aaf9b880c9601e1af96789ea4f43cd72e6ecc4
derekmorgan38 <37957571+derekmorgan38@users.noreply.github.com>
2020-06-17 06:59:50 -0400

    Add files via upload

---
+++ Bike Portal (1).cpp
@@ -0,0 +1,35 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 11.00
+# Visual Studio 2010
+Project("{E24C65DC-7377-472B-9ABA-BC803B73C61A}") = "Bike Portal", "Bike Portal\", "{51CFC558-F926-45C3-B9D8-8880987E96B3}"
+	ProjectSection(WebsiteProperties) = preProject
+		TargetFrameworkMoniker = ".NETFramework,Version%3Dv4.0"
+		Debug.AspNetCompiler.VirtualPath = "/Bike Portal"
+		Debug.AspNetCompiler.PhysicalPath = "Bike Portal\"
+		Debug.AspNetCompiler.TargetPath = "PrecompiledWeb\Bike Portal\"
+		Debug.AspNetCompiler.Updateable = "true"
+		Debug.AspNetCompiler.ForceOverwrite = "true"
+		Debug.AspNetCompiler.FixedNames = "false"
+		Debug.AspNetCompiler.Debug = "True"
+		Release.AspNetCompiler.VirtualPath = "/Bike Portal"
+		Release.AspNetCompiler.PhysicalPath = "Bike Portal\"
+		Release.AspNetCompiler.TargetPath = "PrecompiledWeb\Bike Portal\"
+		Release.AspNetCompiler.Updateable = "true"
+		Release.AspNetCompiler.ForceOverwrite = "true"
+		Release.AspNetCompiler.FixedNames = "false"
+		Release.AspNetCompiler.Debug = "False"
+		VWDPort = "3146"
+	EndProjectSection
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{51CFC558-F926-45C3-B9D8-8880987E96B3}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{51CFC558-F926-45C3-B9D8-8880987E96B3}.Debug|Any CPU.Build.0 = Debug|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+EndGlobal
